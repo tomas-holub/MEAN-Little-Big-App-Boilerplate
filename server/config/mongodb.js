376bc7e514b370ee3da6c65b0862d8244d78a900
tholub <tholub83@gmail.com>
2015-04-13 16:28:31 +0200

    correction of variable name

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -9,7 +9,7 @@ var connectionString = dbHost + ':' + dbPort + '/' + dbName;
 
 // if OPENSHIFT env variables are present, use the available connection info:
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-    connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
     process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
     process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
     process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
