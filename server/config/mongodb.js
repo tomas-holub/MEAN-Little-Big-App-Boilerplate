ab9a3186cc3f95c73d836eaf5fe9bb57ae8dd860
tholub <tholub83@gmail.com>
2015-04-13 16:53:18 +0200

    test

--- server/config/mongodb.js
+++ server/config/mongodb.js
@@ -8,13 +8,15 @@ var dbPort   = config.dbPort || '27017';
 var connectionString = dbHost + ':' + dbPort + '/' + dbName;
 
 // if OPENSHIFT env variables are present, use the available connection info:
-if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
-    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
-    process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
-    process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
-    process.env.OPENSHIFT_APP_NAME;
-}
+//if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
+//    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+//    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+//    process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+//    process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+//    process.env.OPENSHIFT_APP_NAME;
+//}
+
+connectionString = 'admin:Gqxv7VTZ7vfh@' + process.env.OPENSHIFT_MONGODB_DB_HOST + ':' + process.env.OPENSHIFT_MONGODB_DB_PORT + '/' + process.env.OPENSHIFT_APP_NAME;
 
 mongoose.connect('mongodb://' + connectionString);
 
