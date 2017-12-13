496e423745b712f0e047eae77df17c2d5942eb3a
Tomas Holub <tomas.holub@unic.com>
2017-12-13 13:46:28 +0100

    Updates config

--- server/app.js
+++ server/app.js
@@ -6,7 +6,7 @@ var env    = process.env.NODE_ENV || 'development';
 var config = require('./config/config')[env];
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // serves client
 app.use('/', express.static('./public'));
